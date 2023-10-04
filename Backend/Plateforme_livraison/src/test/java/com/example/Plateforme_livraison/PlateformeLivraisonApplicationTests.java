28b8d8ff33dcf94a3d678e57ab9f21a2b4303dc2
samir chemkhi <samirchemkhi09@gmail.com>
2023-10-04 13:21:28 +0100

    add backend

---
+++ Backend/Plateforme_livraison/src/test/java/com/example/Plateforme_livraison/PlateformeLivraisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Plateforme_livraison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlateformeLivraisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
