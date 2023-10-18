98883f3f226034af13ed13528f8f172e19e9e7a9
aliCh10 <130141511+aliCh10@users.noreply.github.com>
2023-10-18 16:49:05 +0100

    page singin and singup

--- Frontend/plateforme_livraison/src/app/app.module.ts
+++ Frontend/plateforme_livraison/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SingInComponent } from './sing-in/sing-in.component';
+import { SingUpComponent } from './sing-up/sing-up.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingInComponent,
+    SingUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
