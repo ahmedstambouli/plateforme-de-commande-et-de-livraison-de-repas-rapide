789ab66dbaea28e3b8813f8fa373dfdd34ad319c
ahmed STANBOULI <ahmed.stambouli98@gmail.com>
2023-10-23 22:33:27 +0100

    partenaire terminer

--- Backend/Plateforme_livraison/src/main/java/com/example/Plateforme_livraison/Models/Partenaire.java
+++ Backend/Plateforme_livraison/src/main/java/com/example/Plateforme_livraison/Models/Partenaire.java
@@ -41,8 +41,8 @@ public class Partenaire {
     private String email;
 
 
-    @NotBlank(message = "password is required")
-    @Size(min=8, message="Password must be at least 8 characters long.")
+    @NotBlank(message = " is required")
+    @Size(min=8, message=" must be at least 8 characters long.")
     private String password;
 
 
--- Backend/Plateforme_livraison/src/main/java/com/example/Plateforme_livraison/config/SecurityConfig.java
+++ Backend/Plateforme_livraison/src/main/java/com/example/Plateforme_livraison/config/SecurityConfig.java
@@ -2,11 +2,15 @@ package com.example.Plateforme_livraison.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +23,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("public/**").permitAll()
                         .anyRequest().authenticated()
+                        
                 );
         http
                 .formLogin();
@@ -30,5 +35,6 @@ public class SecurityConfig {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 
 }
