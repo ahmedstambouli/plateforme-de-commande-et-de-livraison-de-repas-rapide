5efe2501bdba110a1f5a4457ed61efb303e060ef
ahmed STANBOULI <ahmed.stambouli98@gmail.com>
2023-10-23 22:36:49 +0100

    upate

--- Backend/Plateforme_livraison/src/main/java/com/example/Plateforme_livraison/config/SecurityConfig.java
+++ Backend/Plateforme_livraison/src/main/java/com/example/Plateforme_livraison/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
