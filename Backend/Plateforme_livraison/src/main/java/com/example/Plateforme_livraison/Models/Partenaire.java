2869a187389593c28e3505acc207457f50eb9b85
ahmed STANBOULI <ahmed.stambouli98@gmail.com>
2023-11-06 16:32:23 +0100

    update

--- Backend/Plateforme_livraison/src/main/java/com/example/Plateforme_livraison/Models/Partenaire.java
+++ Backend/Plateforme_livraison/src/main/java/com/example/Plateforme_livraison/Models/Partenaire.java
@@ -3,19 +3,18 @@ package com.example.Plateforme_livraison.Models;
 import java.util.*;
 import org.hibernate.validator.constraints.Length;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
+
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
--- Backend/Plateforme_livraison/src/main/java/com/example/Plateforme_livraison/controller/ParteanireController.java
+++ Backend/Plateforme_livraison/src/main/java/com/example/Plateforme_livraison/controller/ParteanireController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
